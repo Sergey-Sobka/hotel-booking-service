088306150fe4560d3fb4a979297925d670f90136
amesemyta1 <amesemyta@gmail.com>
2026-06-13 18:49:06 +0300

    craete: Create User views

--- users/views.py
+++ users/views.py
@@ -1,1 +1,20 @@
-"""User API views will be implemented in their feature tasks."""
+from django.contrib.auth import get_user_model
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from users.serializers import UserRegisterSerializer, UserProfileSerializer
+
+User = get_user_model()
+
+
+class UserRegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRegisterSerializer
+    permission_classes = (AllowAny,)
+
+
+class UserProfileView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserProfileSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
