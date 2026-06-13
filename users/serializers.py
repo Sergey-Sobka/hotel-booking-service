5598b2ef20d50ac980597f2f71b6e252db32381a
amesemyta1 <amesemyta@gmail.com>
2026-06-13 18:48:47 +0300

    craete: Create User serializers

---
+++ users/serializers.py
@@ -0,0 +1,22 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+User = get_user_model()
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=5)
+
+    class Meta:
+        model = User
+        fields = ("id", "email", "first_name", "last_name", "password")
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id", "email", "first_name", "last_name", "is_staff")
+        read_only_fields = ("id", "email", "is_staff")
