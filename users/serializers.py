677cfe8afe9ee63b9205a7ab8cf4684908bbae8b
amesemyta1 <amesemyta@gmail.com>
2026-06-14 18:21:18 +0300

    fix: update serializers and settings for custom Authorize header and hash password in User serializer

--- users/serializers.py
+++ users/serializers.py
@@ -9,10 +9,14 @@ class UserRegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ("id", "email", "first_name", "last_name", "password")
+        fields = ("email", "password")
+        extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
-        return User.objects.create_user(**validated_data)
+        user = User(**validated_data)
+        user.set_password(validated_data["password"])
+        user.save()
+        return user
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
