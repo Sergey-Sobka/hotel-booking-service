a8d8f9ad939b1439b8adc3484b8f27931937e7fc
Aleksandr <32303139+amesemyta1@users.noreply.github.com>
2026-06-14 17:45:10 +0300

    Revert "Feature/users crud auth"

--- config/urls.py
+++ config/urls.py
@@ -3,9 +3,8 @@ URL configuration for config project.
 """
 
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/users/", include("users.urls", namespace="users")),
 ]
