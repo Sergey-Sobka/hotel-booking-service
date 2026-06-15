b300272e1151fff2f7f196194133014e6cfbe022
naomi <orest.chupa@gmail.com>
2026-06-15 18:20:12 +0300

    Add detail and list views for booking

--- bookings/views.py
+++ bookings/views.py
@@ -1,1 +1,26 @@
-"""Booking API views will be implemented in their feature tasks."""
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .models import Booking
+from .serializers import BookingSerializer
+from .filters import BookingFilter
+
+
+class BookingListView(generics.ListAPIView):
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
+    filterset_class = BookingFilter
+
+    def get_queryset(self):
+        return Booking.objects.filter(
+            user=self.request.user
+        ).select_related(
+            "room"
+        )
+
+
+class BookingDetailView(generics.RetrieveAPIView):
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Booking.objects.filter(user=self.request.user)
