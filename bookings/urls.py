18b891fa488786e89ee0c6fa0c0f3af4d96464f4
naomi <orest.chupa@gmail.com>
2026-06-15 18:26:05 +0300

    flake8 fix

--- bookings/urls.py
+++ bookings/urls.py
@@ -4,5 +4,9 @@ from .views import BookingListView, BookingDetailView
 
 urlpatterns = [
     path("bookings/", BookingListView.as_view(), name="booking-list"),
-    path("bookings/<int:pk>/", BookingDetailView.as_view(), name="booking-detail"),
+    path(
+        "bookings/<int:pk>/",
+        BookingDetailView.as_view(),
+        name="booking-detail"
+    ),
 ]
