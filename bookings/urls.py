2f4c78c87f6e457c097ad014ae011a79b28085a4
naomi <orest.chupa@gmail.com>
2026-06-15 18:36:45 +0300

    Add app_name

--- bookings/urls.py
+++ bookings/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import BookingListView, BookingDetailView
 
 
+app_name = "bookings"
+
 urlpatterns = [
     path("bookings/", BookingListView.as_view(), name="booking-list"),
     path(
