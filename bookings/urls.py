bfcf7f4bd6e377f93a79e1d8f35d104d3aa9f29e
lubkoserhii <lubko.serhii@gmail.com.com>
2026-06-16 15:01:58 +0300

    Add urL patterns for booking check-in
    Signed-off-by: lubkoserhii <lubko.serhii@gmail.com.com>

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
-from .views import BookingListView, BookingDetailView
-
+from .views import BookingCheckInView, BookingListView, BookingDetailView
 
 app_name = "bookings"
 
 urlpatterns = [
     path("", BookingListView.as_view(), name="booking-list"),
+    path("<int:pk>/", BookingDetailView.as_view(), name="booking-detail"),
     path(
-        "<int:pk>/",
-        BookingDetailView.as_view(),
-        name="booking-detail"
+        "<int:pk>/check-in/",
+        BookingCheckInView.as_view(),
+        name="booking-check-in",
     ),
 ]
